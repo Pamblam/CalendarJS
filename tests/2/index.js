584a5b9e9d8b1b66c24107bfc34b5e9416bf7e47
Rob <adelphia@protonmail.ch>
2018-12-07 00:27:53 -0500

    separated test logic

---
+++ tests/2/index.js
@@ -0,0 +1,14 @@
+var cal = null;
+var ele = document.getElementById('cal');			
+var opts = {
+	events: [{date: new Date(2016, 8, 11), desc:"My Birthday!"}],
+	abbrDay: false,
+	abbrMonth: true, 
+	month: 9, 
+	year: 2016,
+	onDayClick: function(date){cal.selectDate(date);},
+	onEventClick: function(event){alert(event.desc);}
+};
+cal = new calendar(ele, opts);
+
+window.butts = "test";
